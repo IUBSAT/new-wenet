5f3254c6f1fd5952967151648ccae8328f3f45aa
lbsny <lbsnyder@proton.me>
2024-02-25 16:25:49 -0500

    fix issue with overlay, try using manager for encoding only newest image

--- tx/luke_imagev2.py
+++ tx/luke_imagev2.py
@@ -1,5 +1,5 @@
-from multiprocessing import Process, Queue, Value
-import PacketTX, sys, os, argparse
+from multiprocessing import Process, Queue, Value, Manager
+import PacketTX, sys, os, argparse, glob
 import time
 
 callsign = "KD9ZSC"
@@ -7,19 +7,23 @@ image_range = range(1, 20)
 debug_output = False
 new_size = ["800x608"]
 
-def capture_process(capture_queue):
+def capture_process(capture_list):
     while True:
         for i in image_range:
             cmd = "libcamera-still --immediate -o new_images/%d.jpg" % i
             os.system(cmd)
-            capture_queue.put(i)
-            #time.sleep(1)  # Adjust sleep time based on capture frequency
+            capture_list.append(i)
+            time.sleep(0.5)  # Adjust sleep time based on capture frequency
 
-def ssdv_process(capture_queue, encode_queue, encoded_set, transmitted_set, latest_image):
+def ssdv_process(capture_list, encode_queue, encoded_set, transmitted_set, latest_image):
     while True:
-        if not capture_queue.empty():
-            image_num = capture_queue.get()
+        if capture_list:
+            image_num = capture_list[-1]
+            list_of_files = glob.glob('new_images/*.jpg') # * means all if need specific format then *.csv
+            latest_file = max(list_of_files, key=os.path.getctime)
+            print("Latest jpg from the camera: ",latest_file)
             if image_num > latest_image.value:
+                print("Encoding image {} because it is newer than {}" .format(image_num, latest_image.value))
                 encode_image(image_num)
                 encode_queue.put(image_num)
                 encoded_set.add(image_num)
@@ -35,7 +39,7 @@ def transmit_process(encode_queue, transmitted_set, latest_image):
 
 def encode_image(image_num):
     # Similar to your existing ssdv function
-    #filename = "%d.jpg" % image_num
+    #filename = "new_images/%d.jpg" % image_num
     os.system("cp new_images/%d.jpg new_images/%d_raw.jpg" % (image_num, image_num))
     # Build up our imagemagick 'convert' command line
     overlay_str = "convert new_images/%d.jpg -gamma 0.8 -font Helvetica -pointsize 40 -gravity North " % image_num
@@ -49,6 +53,7 @@ def encode_image(image_num):
 
     #tx.transmit_text_message("Adding overlays to image.")
     os.system(overlay_str)
+    
     for size in new_size:
         os.system(
             "convert new_images/%d.jpg -resize %s\! new_images/%d_%s.jpg" % (
@@ -94,30 +99,33 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    capture_queue = Queue()
-    encode_queue = Queue()
-    encoded_set = set()
-    transmitted_set = set()
-    latest_image = Value('i', 0)
+    with Manager() as manager:
+        capture_list = manager.list()
+        capture_queue =manager.Queue()
+        encode_queue = manager.Queue()
+        encoded_set = manager.set()
+        transmitted_set = manager.set()
+        
+        latest_image = Value('i', 0)
 
-    # Start the capture process
-    capture_process = Process(target=capture_process, args=(capture_queue,))
-    capture_process.start()
+        # Start the capture process
+        capture_process = Process(target=capture_process, args=(capture_queue,))
+        capture_process.start()
 
-    # Start the ssdv process
-    ssdv_process = Process(target=ssdv_process, args=(capture_queue, encode_queue, encoded_set, transmitted_set, latest_image))
-    ssdv_process.start()
+        # Start the ssdv process
+        ssdv_process = Process(target=ssdv_process, args=(capture_queue, encode_queue, encoded_set, transmitted_set, latest_image))
+        ssdv_process.start()
 
-    # Start the transmit process
-    transmit_process = Process(target=transmit_process, args=(encode_queue, transmitted_set, latest_image))
-    transmit_process.start()
+        # Start the transmit process
+        transmit_process = Process(target=transmit_process, args=(encode_queue, transmitted_set, latest_image))
+        transmit_process.start()
 
-    try:
-        capture_process.join()
-        ssdv_process.join()
-        transmit_process.join()
-    except KeyboardInterrupt:
-        print("Terminating processes...")
-        capture_process.terminate()
-        ssdv_process.terminate()
-        transmit_process.terminate()
+        try:
+            capture_process.join()
+            ssdv_process.join()
+            transmit_process.join()
+        except KeyboardInterrupt:
+            print("Terminating processes...")
+            capture_process.terminate()
+            ssdv_process.terminate()
+            transmit_process.terminate()
